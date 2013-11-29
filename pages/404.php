9a578fe6738758e73bae3c1a5645c4c4313c78eb
Felipe Ferreira <felipehfj@gmail.com>
2013-11-29 15:04:33 -0200

    Update 404.php

--- pages/404.php
+++ pages/404.php
@@ -2,7 +2,7 @@
       <div class="row-fluid">
         <div class="span6 offset3">
             <h2>Error 404</h2>
-            <p>The document you're looking for doesn't exist.</p>
+            <p>A página que você está procurando não existe.</p>
         </div>
       </div>
     </div> <!-- container -->
